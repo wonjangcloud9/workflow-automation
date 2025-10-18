6266d2773d5398b6a7d36e6810e93719c6dfdbd6
notwonjang <venuscwj@naver.com>
2025-10-19 00:02:10 +0900

    MA-1 chore: add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-red">
-      Emilia
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <Button variant="outline">Emilia</Button>
     </div>
   );
 }
