6b7ccc4225affe4918e37418c43be218763ada1f
notwonjang <venuscwj@naver.com>
2025-10-21 23:10:24 +0900

    MA-3 feat: integrate Prisma DAL with tRPC rotuers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-import prisma from "@/lib/db";
+import { getQueryClient, trpc } from "@/trpc/server";
+import {dehydrate, HydrationBoundary, useQuery} from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
-const Home = async () => {
-  const users = await prisma.user.findMany();
+const Home = () => {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+
   return (
-    <div>
-      {JSON.stringify(users)}
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<div>Loading...</div>}>
+        <Client />
+        </Suspense>
+      </HydrationBoundary>
     </div>
-  );
-}
+  )
+};
 
 export default Home;
